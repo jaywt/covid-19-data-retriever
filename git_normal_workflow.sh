8146667c8ee2c80cd9fb47a5685d354837284400
jaywen <jaywenjay@gmail.com>
2020-07-27 00:30:05 -0400

    Daily updated: 2020-07-27 00:30:05

--- git_normal_workflow.sh
+++ git_normal_workflow.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 echo "Starting the workflow"
 echo ""
+echo ""
+echo ""
+echo "# last run on `date`" | cat - covid-19-data-retriever.py > temp && mv temp covid-19-data-retriever.p
+python -v covid-19-data-retriever.py
+echo ""
+echo ""
+echo ""
 git add .
 git commit -m "Daily updated: `date +'%Y-%m-%d %H:%M:%S'`"
 git push origin master
+echo ""
+echo ""
+echo ""
+
+echo ""
+echo ""
+echo ""
 echo "**Workflow done**"
